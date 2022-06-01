1635e6f4b6092dc43667bc59b797f4cd1d6e68e6
Siti Zulaiha <rupatsiti12@gmail.com>
2022-06-01 15:09:53 +0700

    Kode program view data

---
+++ app/src/main/java/com/example/viewdata/ListViewActivity.kt
@@ -0,0 +1,20 @@
+package com.example.viewdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_list_view.*
+import kotlinx.android.synthetic.main.activity_list_view.*
+import java.util.*
+class ListviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+        var dataArray: Array<String> =
+            resources.getStringArray(R.array.jurusan)
+        Arrays.sort(dataArray)
+        var adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, dataArray)
+        listview.adapter = adapter
+    }
+}
