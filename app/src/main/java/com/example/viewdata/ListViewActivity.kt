31867d99a82729fcfe6e2d514686df8c06fb8762
Siti Zulaiha <rupatsiti12@gmail.com>
2022-06-23 09:46:31 +0700

    Kode program view data

--- app/src/main/java/com/example/viewdata/ListViewActivity.kt
+++ app/src/main/java/com/example/viewdata/ListViewActivity.kt
@@ -3,13 +3,13 @@ package com.example.viewdata
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.activity_list_view.*
-import kotlinx.android.synthetic.main.activity_list_view.*
+import kotlinx.android.synthetic.main.activity_listview.*
 import java.util.*
-class ListviewActivity : AppCompatActivity() {
+
+class ListViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_view)
+        setContentView(R.layout.activity_listview)
         var dataArray: Array<String> =
             resources.getStringArray(R.array.jurusan)
         Arrays.sort(dataArray)
--- app/src/main/java/com/example/viewdata/MainActivity.kt
+++ app/src/main/java/com/example/viewdata/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnlistview.setOnClickListener{
-            actionPindah(ListviewActivity())
+            actionPindah(ListViewActivity())
         }
     }
     private fun actionPindah(simpleActivity: Any) {
